a79d0c464e26dcf8117fadf95dfde00ad0c437a1
Todd Willey <xtoddx@gmail.com>
2013-06-22 13:19:33 -0400

    Add some day 2 sketeches

---
+++ Day2/BreadboardDebug/BreadboardDebug.ino
@@ -0,0 +1,36 @@
+int ledPin = 13;
+int buttonPin = 8;
+boolean buttonPressed = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(buttonPin, 1);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // pulse the light
+  delay(5000);
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+
+  // check the button
+  buttonPressed = (buttonPressed || (digitalRead(buttonPin) == HIGH));
+  delay(200);
+  buttonPressed = (buttonPressed || (digitalRead(buttonPin) == HIGH));
+  delay(200);
+  buttonPressed = (buttonPressed || (digitalRead(buttonPin) == HIGH));
+  delay(200);
+  buttonPressed = (buttonPressed || (digitalRead(buttonPin) == HIGH));
+  delay(200);
+  buttonPressed = (buttonPressed || (digitalRead(buttonPin) == HIGH));
+  
+  // write the result
+  if (buttonPressed) {
+    Serial.println("OKAY!");
+  } else {
+    Serial.println("I'm not seeing anything from the button :(");
+  }
+}
---
+++ Day2/SerialLightToggle/SerialLightToggle.ino
@@ -0,0 +1,25 @@
+int serialValue = 0;
+int lightOn = false;
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    serialValue = Serial.read();
+  }
+  if (serialValue = '1') {
+    lightOn = true;
+  }
+  if (serialValue = '0') {
+    lightOn = false;
+  }
+  if (lightOn) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
