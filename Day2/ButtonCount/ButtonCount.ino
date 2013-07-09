ad52ebf8d27e5b4d1a15c1dd0a7f2f23fb9281cd
bmarkslash7 <bmarkslash7@hotmail.com>
2013-07-08 22:39:29 -0400

    Added introduction and delay to initialize, changed to normally closed button.

--- Day2/ButtonCount/ButtonCount.ino
+++ Day2/ButtonCount/ButtonCount.ino
@@ -1,12 +1,24 @@
 int buttonPin = 8;
 int count = 0;
-int buttonPressedState = HIGH; // HIGH for normally closed
-int lastState = LOW; // inverse of above for starting out w/o press
+int buttonPressedState = LOW; // HIGH for normally closed, LOW for normally open buttons
+int lastState = HIGH; // inverse of above for starting out w/o press
 
 void setup() {
   pinMode(buttonPin, INPUT);
   digitalWrite(buttonPin, HIGH); // pullup
   Serial.begin(9600);
+  Serial.println("I will count the number of times you press a button");
+  Serial.println("If the counter starts at 1 and not zero, but you have not pressed the button, you need to change buttonPressedState and lastState.");
+  Serial.println("I will wait five seconds before starting.");
+  delay(1000);
+  Serial.println("I will wait four seconds before starting.");
+  delay(1000);
+  Serial.println("I will wait three seconds before starting.");
+  delay(1000);
+  Serial.println("I will wait two seconds before starting.");
+  delay(1000);
+  Serial.println("I will wait one second before starting.");
+  delay(1000);
 }
 
 void loop() {
