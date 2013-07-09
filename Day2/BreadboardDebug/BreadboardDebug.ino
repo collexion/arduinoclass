3020aa2bf3d457787af0fceb3ee4c527a04a6078
bmarkslash7 <bmarkslash7@hotmail.com>
2013-07-08 22:24:58 -0400

    button and pin both connected to ground, so button never fired.

--- Day2/BreadboardDebug/BreadboardDebug.ino
+++ Day2/BreadboardDebug/BreadboardDebug.ino
@@ -1,19 +1,23 @@
+//Use ButtonSchematic.pdf
+// Button appears to connect to ground, HIGH means button not pressed, LOW means button pressed
+// Momentary buttons are usually open (off) when not pressed
 int ledPin = 13;
 int buttonPin = 8;
 boolean buttonPressed = false;
-int pressedButtonState = HIGH; //normally closed
+int pressedButtonState = LOW; //normally closed
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
-  digitalWrite(buttonPin, 1); //pullup
+  digitalWrite(buttonPin, HIGH); //pullup
+  // digitalWrite to an input pin as HIGH turns on internal pullup resistor
   Serial.begin(9600);
   
   //get initial button state
   if (digitalRead(buttonPin) == pressedButtonState) {
     Serial.println("The button is currently pressed.");
-    Serial.println("You may have wired it wrong.");
-    Serial.println("If your button normally open, change pressedButtonState");
+    Serial.println("If you are not pressing it, you may have wired it wrong.");
+    Serial.println("If your button is normally closed, change pressedButtonState to HIGH");
   }
   Serial.println("Initialized");
 }
@@ -29,7 +33,9 @@ void loop() {
 
   // check the button
   read1 = digitalRead(buttonPin);
-  delay(1000);
+  delay(100); // used to be delay(1000);
+  // one second seems like a long time
+  // especially given the fact the LED is on for five seconds.
   read2 = digitalRead(buttonPin);
   if (read1 == read2 && read1 == pressedButtonState) {
     buttonPressed = true;
