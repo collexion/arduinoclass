4297c8299a555bdfbfaa84c8351b75bc525262fe
unknown <Aaron@Spectra.(none)>
2013-07-15 20:50:08 -0400

    Add base Day3 content

---
+++ Day3/Analog_LED_Pulsing/Analog_LED_Pulsing.ino
@@ -0,0 +1,26 @@
+
+int led = 9;           // the pin that the LED is attached to
+int brightness = 0;    // how bright the LED is
+int pulseAmount = 5;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
+void setup()  { 
+  // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
+} 
+
+// the loop routine runs over and over again forever:
+void loop()  { 
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);    
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + pulseAmount;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    pulseAmount = -pulseAmount ; 
+  }     
+  // wait for 30 milliseconds to see the dimming effect    
+  delay(30);                            
+}
